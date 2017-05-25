dd6e09bfc0974c6af3e42eb2ce27c364c944a072
tanmayshankar <tanmayshankar>
2017-05-25 16:27:11 -0400

    added 2 layer support for lstm

--- lstm.py
+++ lstm.py
@@ -16,17 +16,26 @@ class LSTM(nn.Module):
 		self.batch_size = batch_size
 
 
-		self.gru = nn.GRUCell(self.input_size, self.hidden_size)
+		self.gru1 = nn.GRUCell(self.input_size, self.hidden_size)
 		self.h2o = nn.Linear(self.hidden_size, self.output_size)
-		self.h = Variable(torch.zeros(batch_size, hidden_size), requires_grad=True)
+		self.h1 = Variable(torch.zeros(batch_size, hidden_size), requires_grad=True)
+		if n_layers == 2:
+			self.gru2 = nn.GRUCell(self.hidden_size, self.hidden_size)
+			self.h2 = Variable(torch.zeros(batch_size, hidden_size), requires_grad=True)
 
 
 	def forward(self,x):
-		self.h = self.gru(x, self.h)
-		o = self.h2o(self.h)
+		self.h1 = self.gru1(x, self.h1)
+		if self.n_layers == 2:
+			self.h2 = self.gru2(self.h1, self.h2)
+			o = self.h2o(self.h2)
+		else:
+			o = self.h2o(self.h1)
 
 		return o
 
 	def reset(self):
-		self.h = Variable(torch.zeros(self.batch_size, self.hidden_size), requires_grad=True)
+		self.h1 = Variable(torch.zeros(self.batch_size, self.hidden_size), requires_grad=True)
+		if self.n_layers == 2:
+			self.h2 = Variable(torch.zeros(self.batch_size, self.hidden_size), requires_grad=True)
 
